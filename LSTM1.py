bc5aa3bc6fa8589e6a2eaf3484fe3e4f2a43b804
naveen317 <42955965+naveen317@users.noreply.github.com>
2018-10-30 15:40:45 +0530

    Add files via upload

---
+++ LSTM1.py
@@ -0,0 +1,170 @@
+
+# coding: utf-8
+
+# In[29]:
+
+
+import numpy
+from keras.models import Sequential
+from keras.layers import Dense,Dropout
+from keras.layers import LSTM
+from keras.utils import np_utils
+
+
+# In[30]:
+
+
+# fix random seed for reproducibility
+numpy.random.seed(7)
+
+
+# In[31]:
+
+
+# define the raw dataset
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# create mapping of characters to integers (0-25) and the reverse
+char_to_int = dict((c, i) for i, c in enumerate(alphabet))
+int_to_char = dict((i, c) for i, c in enumerate(alphabet))
+
+
+# In[4]:
+
+
+char_to_int
+
+
+# ## This step we are creating the sequance of letters ...
+
+# In[32]:
+
+
+# prepare the dataset of input to output pairs encoded as integers
+
+
+seq_length = 1
+dataX = []
+dataY = []
+for i in range(0, len(alphabet) - seq_length, 1):
+    seq_in = alphabet[i:i + seq_length]
+    seq_out = alphabet[i + seq_length]
+    dataX.append([char_to_int[char] for char in seq_in])
+    dataY.append(char_to_int[seq_out])
+    print(seq_in, '->', seq_out)
+    
+
+
+# In[6]:
+
+
+dataX
+
+
+# In[33]:
+
+
+# reshape X to be [samples, time steps, features]
+X = numpy.reshape(dataX, (len(dataX), seq_length, 1))
+
+
+# In[8]:
+
+
+X
+
+
+# In[34]:
+
+
+# normalize
+X = X / float(len(alphabet))
+
+
+# In[10]:
+
+
+dataY
+
+
+# In[11]:
+
+
+y
+
+
+# In[35]:
+
+
+# one hot encode the output variable
+y = np_utils.to_categorical(dataY)
+
+
+# In[13]:
+
+
+y
+
+
+# In[36]:
+
+
+input_shape=(X.shape[1], X.shape[2])
+
+
+# In[37]:
+
+
+input_shape
+
+
+# In[38]:
+
+
+# create and fit the model
+model = Sequential()
+model.add(LSTM(32, input_shape=(X.shape[1], X.shape[2])))
+model.add(Dropout(0.2))
+model.add(Dense(y.shape[1], activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(X, y, epochs=500, batch_size=1, verbose=2)
+
+
+# In[39]:
+
+
+model.summary()
+
+
+# In[40]:
+
+
+# summarize performance of the model
+scores = model.evaluate(X, y, verbose=0)
+print("Model Accuracy: %.2f%%" % (scores[1]*100))
+
+
+# In[18]:
+
+
+dataX
+
+
+# In[19]:
+
+
+result = int_to_char[index]
+
+
+# In[41]:
+
+
+# demonstrate some model predictions
+for pattern in dataX:
+    x = numpy.reshape(pattern, (1, len(pattern), 1))
+    x = x / float(len(alphabet))
+    prediction = model.predict(x, verbose=0)
+    index = numpy.argmax(prediction)
+    result = int_to_char[index]
+    seq_in = [int_to_char[value] for value in pattern]
+    print(seq_in, "->", result)
+
